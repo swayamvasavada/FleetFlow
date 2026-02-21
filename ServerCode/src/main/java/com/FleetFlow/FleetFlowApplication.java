28f2036fcc5b586b765c33fd68b713d4ed22837e
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2026-02-21 09:56:04 +0530

    Initial Project Setup

---
+++ ServerCode/src/main/java/com/FleetFlow/FleetFlowApplication.java
@@ -0,0 +1,13 @@
+package com.FleetFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetFlowApplication.class, args);
+	}
+
+}
---
+++ ServerCode/src/test/java/com/FleetFlow/FleetFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FleetFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
